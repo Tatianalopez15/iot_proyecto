374faf3d0008953d380dd99e9bcb9fd24afa0af2
Tatiana <tatiqnahenao@gmail.com>
2025-09-18 19:58:12 -0500

    nuevo

---
+++ database/migrations/2025_09_24_013702_create_departaments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departaments', function (Blueprint $table) {
+          $table->id();
+        $table->string('name');
+        $table->string('code')->unique();
+        $table->string('abbrev', 10)->nullable();
+        $table->boolean('status')->default(true);
+        $table->foreignId('id_country')->constrained('countries');
+        $table->timestamps();
+        $table->softDeletes();
+    });
+
+}
+ 
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departaments');
+    }
+};
